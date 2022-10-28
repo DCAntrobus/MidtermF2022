6de1673420447187c4c3e1692656a3d7b7220b77
Desmond Antrobus <dantrobu@my.centennialcollege.ca>
2022-10-27 22:07:02 -0400

    All fixes complete

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//Mongo Atlas database implementation
+export const MongoURI = "mongodb+srv://Desmond:Golda123@midtermcomp229f2022.uxvuwat.mongodb.net/books?retryWrites=true&w=majority";
 export const Secret = "someSecret";
